0780155ffcd004b69a93789c3408a7ab641c75a4
Your Name <dangcongnhat2004qb@gmail.com>
2023-06-02 15:30:49 +0700

    Nhat commit

---
+++ src/test/java/com/bwd/Bwd2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bwd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bwd2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
